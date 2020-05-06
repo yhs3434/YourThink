b312955a2d0e55fb00b23e1d0ef7233ea56fda95
Yoon Han Sol <yoonhs3434@naver.com>
2020-05-06 20:09:45 +0900

    ssl...

---
+++ server_socket/app2.js
@@ -0,0 +1,10 @@
+// ssl 적용하기 위한 서버
+// letsencrypt 이용.
+
+var http = require('http');
+
+http.createServer(function (request, response) {  
+    response.writeHead(200, {'Content-Type' : 'text/plain'});
+    response.write('Hello nodejs');
+    response.end();
+}).listen(8080);
